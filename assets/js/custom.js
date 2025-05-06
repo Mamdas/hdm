24fd35fa96ce5692611d53145b1793209e2d1d96
Mamdas <mkohansal47@gmail.com>
2025-05-06 16:00:41 +0330

    new

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,4 +20,28 @@ jQuery(document).ready(function () {
             top: mouseY - 40 + "px"
         });
     });
+
+
+
+
+    $(window).on("scroll", function () {
+        var sectionOffset = $(".section-creative").offset().top;
+        var scrollTop = $(window).scrollTop();
+        var distance = scrollTop - sectionOffset;
+
+        if (distance < 0) return;
+
+        var maxScroll = 300; // حداکثر فاصله‌ای که تغییر اعمال میشه
+        var progress = Math.min(distance / maxScroll, 1); // عدد بین 0 تا 1
+
+        var rotateX = 35 * (1 - progress); // از 35 به 0
+        var opacity = 0.5 + 0.5 * progress; // از 0.5 به 1
+
+        $(".dashboard").css({
+            "transform": "perspective(1200px) rotateX(" + rotateX + "deg) rotateY(0)",
+            "opacity": opacity
+        });
+    });
+
+
 });
