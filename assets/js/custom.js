678f79feda7ec711179ba19c81d6a73f64c57376
Mamdas <mkohansal47@gmail.com>
2025-05-06 18:24:46 +0330

    new sections

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,4 +44,58 @@ jQuery(document).ready(function () {
     });
 
 
+
+    $(window).on("scroll", function () {
+        var sectionOffset = $(".section-date").offset().top;
+        var scrollTop = $(window).scrollTop();
+        var distance = scrollTop - sectionOffset;
+
+        if (distance < 0) return;
+
+        var maxScroll = 300; // حداکثر فاصله‌ای که تغییر اعمال میشه
+        var progress = Math.min(distance / maxScroll, 1); // عدد بین 0 تا 1
+
+        var rotateX = 35 * (1 - progress); // از 35 به 0
+
+        $(".date__wrap").css({
+            "transform": "perspective(1200px) rotateX(" + rotateX + "deg) rotateY(0)"
+        });
+    });
+
+
+
+
+
+
+    $(window).on("scroll", function () {
+        var $elem = $(".date__wrap");
+        var elemTop = $elem.offset().top;
+        var elemHeight = $elem.outerHeight();
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var scrollBottom = scrollTop + windowHeight;
+
+        var start = elemTop - windowHeight;
+        var end = elemTop + elemHeight - windowHeight;
+
+
+        var progress = (scrollTop - start) / (end - start);
+        progress = Math.min(Math.max(progress, 0), 1);
+
+        // محاسبه‌ی مقادیر نهایی
+        var opacity = 0.5 + (0.5 * progress);
+        var translateY = 44 * (1 - progress);
+        var scale = 0.8 + (0.2 * progress);
+        var rotateX = 22 * (1 - progress);
+
+
+
+        if (scrollBottom >= elemTop) {
+            $elem.css({
+                opacity: opacity,
+                transform: `perspective(1200px) translateY(${translateY}px) scale(${scale}) rotateX(${rotateX}deg)`
+            });
+        }
+    });
+
 });
