a6a7cbca9ead22586733a6229032d7f47175576e
Mamdas <mkohansal47@gmail.com>
2025-05-12 09:32:59 +0330

    responsive added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,28 +1,5 @@
 jQuery(document).ready(function () {
-    var $tooltip = $("#tooltip");
     var $section = $("#section-marquee");
-
-    $section.find("a").on("mouseenter", function () {
-        $tooltip.show();
-    });
-
-    $section.find("a").on("mouseleave", function () {
-        $tooltip.hide();
-    });
-
-    $section.find("a").on("mousemove", function (e) {
-        var offset = $section.offset();
-        var mouseX = e.pageX - offset.left;
-        var mouseY = e.pageY - offset.top;
-
-        $tooltip.css({
-            left: mouseX + 10 + "px",
-            top: mouseY - 40 + "px"
-        });
-    });
-
-
-
 
     $(window).on("scroll", function () {
         var sectionOffset = $(".section-creative").offset().top;
